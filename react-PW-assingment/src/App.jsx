3ab133804af08e265591f2100ade4fb2598fea54
MahatoAmaR2 <bauaa476@gmail.com>
2023-07-26 13:41:36 +0530

    pwreactassingmnet1

---
+++ react-PW-assingment/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return (
+    <>
+  <h2>Jai Sri Ram</h2>
+    </>
+  )
+}
+
+export default App
---
+++ react-PW-assingment/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Person from './component/assingment1/Question1.jsx'
+import Button from './component/assingment1/Question2.jsx'
+import Header from './component/assingment1/Question3.jsx'
+import List from './component/assingment1/Question4.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Person name="BauAA" age="18" />
+  <Button text="Click me!" onClick={()=> console.log("Button clicked")} />
+  <Header title="Welcome to my website!" />
+  <List items={['apple', 'banana', 'orange']} />
+  </React.StrictMode>,
+)
